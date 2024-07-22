30a752ced3be0c033c2c18db795511fe4a6e7bb6
Tehuel <torresbaldi.tehuel@gmail.com>
2024-07-22 17:21:02 -0300

    addGradeForm

---
+++ src/components/AddGradeForm.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+import { useGradesStore } from '@/stores/grades';
+
+const { studentId, excerciseId } = defineProps(['studentId', 'excerciseId'])
+
+const { addGrade } = useGradesStore()
+
+const EMPTY_GRADE = {
+  number: '',
+  comment: ''
+}
+
+const grade = ref({ ...EMPTY_GRADE })
+
+function handleSubmit() {
+  addGrade(
+    studentId,
+    excerciseId,
+    { ...grade.value }
+  )
+  // reset current grade
+  grade.value = { ...EMPTY_GRADE }
+}
+
+const uniqueName = `${studentId}-${excerciseId}`.toLocaleLowerCase();
+
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <div>
+      <label :for="`${uniqueName}-number`">Grade</label>
+      <input type="number" min="1" max="10" :id="`${uniqueName}-number`" v-model="grade.number" />
+    </div>
+
+    <div>
+      <label :for="`${uniqueName}-comment`">Comment</label>
+      <input type="text" :id="`${uniqueName}-comment`" v-model="grade.comment" />
+    </div>
+
+    <button type="submit">Grade</button>
+  </form>
+</template>
--- src/components/GradesStudentsMatrix.vue
+++ src/components/GradesStudentsMatrix.vue
@@ -3,22 +3,12 @@ import { storeToRefs } from 'pinia'
 import { useGradesStore } from '../stores/grades'
 import { useStudentsStore } from '../stores/students'
 import { useExcercisesStore } from '../stores/excercises'
+import AddGradeForm from './AddGradeForm.vue'
 
-const { addGrade, getGradeForStudentAndExcercise } = useGradesStore()
+const { getGradeForStudentAndExcercise } = useGradesStore()
 const { list: studentsList } = storeToRefs(useStudentsStore())
 const { list: excercisesList } = storeToRefs(useExcercisesStore())
 
-const COMMENTS_LIST = ['good', 'bad', 'excellent']
-
-const updateGrade = (student, excercise) => {
-  const number = Math.ceil(Math.random() * 10)
-  const comment = COMMENTS_LIST.at(Math.floor(Math.random() * COMMENTS_LIST.length))
-  const grade = {
-    number,
-    comment
-  }
-  addGrade(student.firstName, excercise.path, grade)
-}
 </script>
 
 <template>
@@ -30,8 +20,9 @@ const updateGrade = (student, excercise) => {
     <tr v-for="st in studentsList" :key="st">
       <td>{{ st.firstName }}</td>
       <td v-for="ex in excercisesList" :key="{ ...ex, ...st }">
-        <pre>{{ getGradeForStudentAndExcercise(st.firstName, ex.path) }}</pre>
-        <button @click="updateGrade(st, ex)">random grade</button>
+        <add-grade-form :studentId="st.firstName" :excerciseId="ex.path" />
+        <hr>
+        <pre>Grade: {{ getGradeForStudentAndExcercise(st.firstName, ex.path) }}</pre>
       </td>
     </tr>
   </table>
