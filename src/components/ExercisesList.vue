41032ad0dcab7f008e6a0eab5e4c507594cf8993
Tehuel <torresbaldi.tehuel@gmail.com>
2024-07-22 22:50:02 -0300

    remove student and exercises

--- src/components/ExercisesList.vue
+++ src/components/ExercisesList.vue
@@ -1,15 +1,39 @@
 <script setup>
 import { storeToRefs } from 'pinia'
 import { useExercisesStore } from '@/stores/exercises'
+import { useGradesStore } from '@/stores/grades';
 
 const exercisesStore = useExercisesStore()
+const gradesStore = useGradesStore()
 const { list: exercises } = storeToRefs(exercisesStore)
+
+function handleRemove(exerciseId) {
+  exercisesStore.removeExercise(exerciseId);
+  gradesStore.removeGradesForExercise(exerciseId);
+}
 </script>
 
 <template>
   <ul class="exercisesList">
     <li v-for="ex in exercises" :key="`${ex.title}${ex.path}`">
       <pre>{{ ex }}</pre>
+      <button @click="handleRemove(ex.path)">x</button>
     </li>
   </ul>
 </template>
+
+<style lang="scss">
+ul.exercisesList {
+  li {
+    list-style: none;
+    list-style-position: outside;
+    margin-bottom: 1rem;
+    display: flex;
+  }
+
+  pre {
+    margin: 0;
+    flex-grow: 1;
+  }
+}
+</style>
--- src/components/StudentsList.vue
+++ src/components/StudentsList.vue
@@ -1,15 +1,39 @@
 <script setup>
 import { storeToRefs } from 'pinia'
 import { useStudentsStore } from '@/stores/students'
+import { useGradesStore } from '@/stores/grades';
 
 const studentsStore = useStudentsStore()
+const gradesStore = useGradesStore()
 const { list: students } = storeToRefs(studentsStore)
+
+function handleRemove(studentId) {
+  studentsStore.removeStudent(studentId);
+  gradesStore.removeGradesForStudent(studentId);
+}
 </script>
 
 <template>
   <ul class="studentsList">
     <li v-for="st in students" :key="`${st.firstName}${st.lastName}`">
       <pre>{{ st }}</pre>
+      <button @click="handleRemove(st.firstName)">x</button>
     </li>
   </ul>
 </template>
+
+<style lang="scss">
+ul.studentsList {
+  li {
+    list-style: none;
+    list-style-position: outside;
+    margin-bottom: 1rem;
+    display: flex;
+  }
+
+  pre {
+    margin: 0;
+    flex-grow: 1;
+  }
+}
+</style>
