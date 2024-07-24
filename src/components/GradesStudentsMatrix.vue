1fc3f38444893f4a418c4779bd2c1eb3f72d60b2
Tehuel <torresbaldi.tehuel@gmail.com>
2024-07-24 10:32:58 -0300

    filter students

--- src/components/GradesStudentsMatrix.vue
+++ src/components/GradesStudentsMatrix.vue
@@ -4,10 +4,40 @@ import { useGradesStore } from '@/stores/grades'
 import { useStudentsStore } from '@/stores/students'
 import { useExercisesStore } from '@/stores/exercises'
 import AddGradeForm from './AddGradeForm.vue'
+import { ref } from 'vue'
 
 const { getGradeForStudentAndExercise } = useGradesStore()
 const { list: studentsList } = storeToRefs(useStudentsStore())
 const { list: exercisesList } = storeToRefs(useExercisesStore())
+
+const appliedFiltersForStudentsList = ref(new Set());
+const appliedFiltersForExercicesList = ref([]);
+
+const filteredExercisesList = ref([...exercisesList.value]);
+
+function isStudentInFilter(studentId) {
+  return appliedFiltersForStudentsList.value.has(studentId);
+}
+
+function applyStudentsListFilter(studentId) {
+  if (isStudentInFilter(studentId)) {
+    appliedFiltersForStudentsList.value.delete(studentId)
+  } else {
+    appliedFiltersForStudentsList.value.add(studentId);
+  }
+}
+const filteredStudentsList = () => {
+  return studentsList.value.filter(st => {
+    if (!appliedFiltersForStudentsList.value.size) {
+      return true
+    }
+    return isStudentInFilter(st.id);
+  })
+};
+
+function applyExercicesListFilter(exercise) {
+  console.log("applyExercicesListFilter", exercise)
+}
 
 </script>
 
@@ -17,12 +47,20 @@ const { list: exercisesList } = storeToRefs(useExercisesStore())
       <thead>
         <tr>
           <th scope="col">Student</th>
-          <th scope="col" v-for="ex in exercisesList" :key="ex">{{ ex.path }}</th>
+          <th scope="col" v-for="ex in exercisesList" :key="ex">
+            {{ ex.path }}
+            <button @click="applyExercicesListFilter(ex)" style="padding: 0; font-size: 0.8em;">filter</button>
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="st in studentsList" :key="st">
-          <th scope="row">{{ st.firstName }}</th>
+        <tr v-for="st in filteredStudentsList()" :key="st">
+          <th scope="row">
+            {{ st.firstName }}
+            <button @click="applyStudentsListFilter(st.id)" style="padding: 0; font-size: 0.8em;">
+              {{ isStudentInFilter(st.id) ? 'show all' : 'solo' }}
+            </button>
+          </th>
           <td v-for="ex in exercisesList" :key="{ ...ex, ...st }">
             <add-grade-form :studentId="st.id" :exerciseId="ex.id" />
             <hr>
