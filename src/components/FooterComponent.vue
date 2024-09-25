8a1c07bee3cd3ce1cf303a817e09a23c9b573cbc
Tehuel <torresbaldi.tehuel@gmail.com>
2024-09-25 19:16:04 +0000

    test commit

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="container">
     <hr>
-    <small>Grade Helper | <a href="https://github.com/tehuel/vue-grade-helper">Source Code</a></small>
+    <small>Vue Grade Helper | <a href="https://github.com/tehuel/vue-grade-helper">Source Code</a></small>
   </footer>
 </template>
