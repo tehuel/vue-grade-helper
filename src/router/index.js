0b2d2e7f219853747ad84a5d3ef1cb3fa0f8d580
Diego Pezet <74683374+diegohpezet@users.noreply.github.com>
2024-10-03 21:05:19 -0300

    use same path structure on router file

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import StudentsView from '../views/StudentsView.vue'
 import ExercisesView from '../views/ExercisesView.vue'
 import GradesView from '../views/GradesView.vue'
-import CoursesView from '@/views/CoursesView.vue'
+import CoursesView from '../views/CoursesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
