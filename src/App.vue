ff6e5c19b957d2a3e4b1432062f18ef5c1bb79de
diegohpezet <diegohpezet@gmail.com>
2024-11-25 21:16:13 -0300

    implement router view with new layouts

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import MainNavbar from './components/MainNavbar.vue'
-import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
-  <main class="container">
-    <main-navbar />
-    <RouterView />
-  </main>
-  <FooterComponent />
+  <RouterView />
 </template>
