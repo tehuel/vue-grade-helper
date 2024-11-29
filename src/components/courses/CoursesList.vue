183edf5d2246ecc22f2944057c5ba0b45a0796cd
diegohpezet <diegohpezet@gmail.com>
2024-11-29 09:58:37 -0300

    course view redesign

--- src/components/courses/CoursesList.vue
+++ src/components/courses/CoursesList.vue
@@ -1,22 +1,32 @@
 <script setup>
-import { storeToRefs } from 'pinia'
+import { storeToRefs } from 'pinia';
 import { useCoursesStore } from '@/stores/courses';
+import { RouterLink } from 'vue-router';
 
-const coursesStore = useCoursesStore()
-const { list: courses } = storeToRefs(coursesStore)
+const coursesStore = useCoursesStore();
+const { list: courses } = storeToRefs(coursesStore);
 
 function handleRemove(courseId) {
-  coursesStore.removeCourse(courseId)
+  coursesStore.removeCourse(courseId);
 }
 </script>
 
 <template>
-  <ul class="coursesList">
-    <li v-for="course in courses" :key="course">
-      <pre>{{ course }}</pre>
-      <button @click="handleRemove(course.id)">x</button>
-    </li>
-  </ul>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
+  <div class="col" v-for="course in courses" :key="course.id">
+    <div class="card h-100">
+      <div class="card-body row">
+        <div class="col">
+          <RouterLink :to="`/courses/${course.id}`" class="card-title fs-4">{{ course.name }}</RouterLink>
+          <p class="card-text text-muted">{{ course.school_year }}</p>
+        </div>
+        <div class="col d-flex justify-content-end">
+          <button class="btn btn-outline-danger" @click="handleRemove(course.id)">x</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <style lang="scss">
